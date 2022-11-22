697926de19e3167904d1ba399c348b97416b9e1e
okada1231 <95010965+okada1231@users.noreply.github.com>
2022-11-22 15:41:43 +0900

    Update SE_app.py

--- SE_app.py
+++ SE_app.py
@@ -9,21 +9,7 @@ import re
 import copy
 import streamlit as st
 
-# パスを取得するのに必要なライブラリをインポート
-import subprocess
-
 tagger = MeCab.Tagger()
-tagger
-
-# 辞書(mecab-ipadic-NEologd)のPathを取得
-cmd=' git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git'
-cmd='echo yes | mecab-ipadic-neologd/bin/install-mecab-ipadic-neologd -n -a '
-cmd='brew link --overwrite mecab'
-cmd='echo `mecab-config --dicdir`"/mecab-ipadic-neologd"'
-path = (subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True).communicate()[0]).decode('utf-8')
-
-# MeCabの事前設定（辞書ファイルをオプションで指定）
-tagger = MeCab.Tagger("-d {0}".format(path))
 
 # 分かち書き用tokenizer
 tokenizer = BertJapaneseTokenizer.from_pretrained('cl-tohoku/bert-base-japanese-whole-word-masking')
