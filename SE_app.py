e28e4723079a41236d3b2b29dfb248facc8e91b8
okada1231 <95010965+okada1231@users.noreply.github.com>
2022-11-22 15:47:20 +0900

    Update SE_app.py

--- SE_app.py
+++ SE_app.py
@@ -1,4 +1,3 @@
-import MeCab
 import pandas as pd
 import numpy as np
 import torch
@@ -9,7 +8,6 @@ import re
 import copy
 import streamlit as st
 
-tagger = MeCab.Tagger()
 
 # 分かち書き用tokenizer
 tokenizer = BertJapaneseTokenizer.from_pretrained('cl-tohoku/bert-base-japanese-whole-word-masking')
@@ -62,81 +60,63 @@ def result():
 
 def main():
     st.title("検索システム（仮)")
-    activities = ["データ確認", "検索画面"]
-    choice = st.sidebar.selectbox("Select Activity", activities)
 
-    if choice == "データ確認":
+    uploaded_file = st.sidebar.file_uploader("訓練用データのアップロード", type='csv')
+    
 
-        uploaded_file = st.sidebar.file_uploader("訓練用データのアップロード", type='csv')
+    if uploaded_file is not None:
 
-        if uploaded_file is not None:
+        ufile = copy.deepcopy(uploaded_file)
 
-            ufile = copy.deepcopy(uploaded_file)
+        try:
+            # 文字列の判定
+            pd.read_csv(ufile, encoding="utf_8_sig")
+            enc = "utf_8_sig"
+        except:
+            enc = "shift-jis"
 
-            try:
-                # 文字列の判定
-                pd.read_csv(ufile, encoding="utf_8_sig")
-                enc = "utf_8_sig"
-            except:
-                enc = "shift-jis"
+        finally:
+            # データフレームの読み込み
+            df = pd.read_csv(uploaded_file, encoding=enc) 
+            pd.set_option("display.max_colwidth", 500)
 
-            finally:
-                # データフレームの読み込み
-                df = pd.read_csv(uploaded_file, encoding=enc) 
-                pd.set_option("display.max_colwidth", 500)
+            df_data = df
 
-                df_data = df
+            # 比較用データリスト
+            data_list = []
+            for index, data in df_data.iterrows():
+                data_list.append(data['質問事項'] + data['回答'])
 
-                # 比較用データリスト
-                data_list = []
-                for index, data in df_data.iterrows():
-                    data_list.append(data['質問事項'] + data['回答'])
+            # 表示用テキストリスト
+            text_list = []
+            for index, data in df_data.iterrows():
+                text_list.append(data['回答'])
 
-                # 表示用テキストリスト
-                text_list = []
-                for index, data in df_data.iterrows():
-                    text_list.append(data['回答'])
+            target_layer = -1
 
-                target_layer = -1
+            word_vec_list =  []
+            for i in range(len(data_list)):
+                input_ids = tokenizer.encode(data_list[i], return_tensors='pt')  # ptはPyTorchの略
+                layers = model(input_ids)
+                layer = layers[0]
+                word_vec = layer[0][target_layer]
+                word_vec_list.append(word_vec)
 
-                word_vec_list =  []
-                for i in range(len(data_list)):
-                    input_ids = tokenizer.encode(data_list[i], return_tensors='pt')  # ptはPyTorchの略
-                    layers = model(input_ids)
-                    layer = layers[0]
-                    word_vec = layer[0][target_layer]
-                    word_vec_list.append(word_vec)
+            # データフレームをセッションステートに退避（名称:df）
+            st.session_state.df = copy.deepcopy(df)
+            st.session_state.dl = copy.deepcopy(data_list)
+            st.session_state.tl = copy.deepcopy(text_list)
+            st.session_state.wv = copy.copy(word_vec_list)
 
-                # データフレームをセッションステートに退避（名称:df）
-                st.session_state.df = copy.deepcopy(df)
-                st.session_state.dl = copy.deepcopy(data_list)
-                st.session_state.tl = copy.deepcopy(text_list)
-                st.session_state.wv = copy.copy(word_vec_list)
+            st.text_input("検索入力欄", key="search")
+            st.caption("入力例（資金調達　方法）（起業　資金）")
+            if st.button("検索"):
+                result()
 
-                # スライダーの表示（表示件数）
-                cnt = st.sidebar.slider('表示する件数', 1, len(df), 10)
-
-                # テーブルの表示
-                st_display_table(df.head(int(cnt)))
-
-        else:
-            st.subheader('訓練用データをアップロードしてください')
+    else:
+        st.subheader('訓練用データをアップロードしてください')
         
-    if choice == "検索画面":
-
-                # セッションステートにデータフレームがあるかを確認
-            if 'df' in st.session_state:
-
-                # セッションステートに退避していたデータフレームを復元
-                df = copy.deepcopy(st.session_state.df)
-
-                st.text_input("検索入力欄", key="search")
-                st.caption("入力例（資金調達　方法）（起業　資金）")
-                if st.button("検索"):
-                    result()
                 
-            else:
-                st.subheader('訓練用データをアップロードしてください')
 
 if __name__ == "__main__":
     main()
